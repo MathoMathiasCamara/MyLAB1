1e43409c94eb33b28b2cfda49a54d66d3959b787
matho <matho@DESKTOP-2OSSN0E>
2019-03-17 16:18:34 +0200

    sd

---
+++ MyLAB1/CubicOperation.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+
+namespace MyLAB1
+{
+    public class CubicOperation
+    {
+        //my varibles 
+        private int _numberOne;
+        private int _numberTwo;
+
+
+        public int NumberOne
+        {
+            get
+            {
+                return _numberOne;
+            }
+            set
+            {
+                _numberOne = value;
+            }
+        }
+        public int NumberTwo
+        {
+            get
+            {
+                return _numberTwo;
+            }
+            set
+            {
+                _numberTwo = value;
+            }
+        }
+        
+
+        //methodes
+        public int Substraction()
+        {
+            return (NumberOne - NumberTwo);
+        }
+
+        //cubic operation
+        public int Cube()
+        {
+            return (CubicFunction(Substraction()));
+        }
+
+
+        //print method
+        public void Print()
+        {
+            Console.Write(_numberOne);
+            Console.Write(_numberTwo);
+        }
+
+        //print method with a message
+        public void Print(string message)
+        {
+            Console.Write(message);
+            Console.Write(_numberOne);
+            Console.Write(_numberTwo);
+        }
+
+        //my exponatial function
+        private int CubicFunction(int value)
+        {
+            return value*value*value;
+        }
+    }
+}
--- MyLAB1/Program.cs
+++ MyLAB1/Program.cs
@@ -10,6 +10,25 @@ namespace MyLAB1
     {
         static void Main(string[] args)
         {
+           //my local variables
+            CubicOperation Operation1 = new CubicOperation();
+            int a = 4, b = 9;
+
+            //operations
+            Console.Write("the cube function of the difference of two integers:\n\n");
+           
+            Operation1.NumberOne = a;          
+            Operation1.NumberTwo = b;
+
+            Console.Write("-----Difference-----\n");
+            Console.Write("{0}-{1}={2}\n", Operation1.NumberOne, Operation1.NumberTwo,Operation1.Substraction());
+
+            Console.Write("----- Results of the cubic operation (a-b)^3  :-----\n");
+            Console.Write("{0}\n\n", Operation1.Cube());
+
+            Console.Write("Your initial values are:\n");
+            Console.Write("a={0}\nb={1}", Operation1.NumberOne,Operation1.NumberTwo);
+            Console.Read();
         }
     }
 }
